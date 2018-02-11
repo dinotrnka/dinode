28ce5fb3c1e3d2fc10bfeafe05e969c535365f0f
Dino Trnka <dinotrnka@gmail.com>
2018-02-11 20:59:36 +0100

    implemented file upload to AWS S3

--- server/controllers/notes.js
+++ server/controllers/notes.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const _ = require('lodash'); // eslint-disable-line more-naming-conventions/snake-case-variables
 const { check, validationResult } = require('express-validator/check');
+const AWS = require('aws-sdk');
 
 const { authenticate } = require('../middleware/authenticate');
 const { Note } = require('../models/note');
 
 const app = express();
+
+AWS.config.loadFromPath('server/config/aws.json');
+const s3_bucket = new AWS.S3({ params: { Bucket: 'dinode/avatars' } });
+
+app.post('/upload', async (req, res) => {
+  if (!req.files) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  const { file } = req.files;
+
+  console.log('FAJL', file);
+
+  const image = { Key: file.name, Body: file.data };
+  s3_bucket.putObject(image, (err, data) => {
+    if (err) {
+      console.log('Error uploading data: ', err);
+      return res.send(err);
+    }
+
+    console.log('succesfully uploaded the image!');
+    return res.send(data);
+  });
+});
 
 app.post('/', authenticate, [
   check('text').exists().withMessage('Text is required'),
--- server/server.js
+++ server/server.js
@@ -3,14 +3,17 @@ require('./db/mongoose');
 
 const express = require('express');
 const body_parser = require('body-parser');
+const file_upload = require('express-fileupload');
+
 const routes = require('./controllers');
 
 const app = express();
 const port = process.env.PORT;
+
 app.use(body_parser.json());
+app.use(file_upload());
 app.use('/api/v1', routes);
 app.listen(port, () => {
-  // eslint-disable-next-line no-console
   console.log(`Started on port ${port}.`);
 });
 
