6d0e4e7266b6afd61b81620d43e84a50ea6fc435
Dino Trnka <dinotrnka@gmail.com>
2018-05-04 15:10:08 +0200

    Allow Cross Domain for local testing

--- server/server.js
+++ server/server.js
@@ -10,6 +10,14 @@ const routes = require('./controllers');
 const app = express();
 const port = process.env.PORT;
 
+const allow_cross_domain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
+
+app.use(allow_cross_domain);
 app.use(body_parser.json());
 app.use(file_upload());
 app.use('/api/v1', routes);
